9e8a6ab37b35f70233b68c6edfda09099b418e73
anoob <anoob@sina.cn>
2024-11-15 00:57:07 +0800

    sub

--- sub_cmake/MeshFEMDependencies.cmake
+++ sub_cmake/MeshFEMDependencies.cmake
@@ -29,7 +29,7 @@ find_package(Threads REQUIRED) # provides Threads::Threads
 # Boost library
 if(NOT TARGET meshfem::boost)
     # Try to use system boost; downloading it with cmake-boost downloads around 800MB of stuff...
-    find_package(Boost 1.54 COMPONENTS filesystem system program_options QUIET)
+    find_package(Boost 1.83 COMPONENTS filesystem system program_options REQUIRED)
     if (NOT Boost_FOUND)
         include(boost)
     endif()
@@ -50,6 +50,7 @@ if(NOT TARGET meshfem::boost)
 endif()
 
 # Catch2
+find_package(Catch2 REQUIRED)
 if(NOT TARGET Catch2::Catch2 AND (CMAKE_SOURCE_DIR STREQUAL PROJECT_SOURCE_DIR))
     meshfem_download_catch()
     add_subdirectory(${MESHFEM_EXTERNAL}/Catch2)
@@ -57,6 +58,7 @@ if(NOT TARGET Catch2::Catch2 AND (CMAKE_SOURCE_DIR STREQUAL PROJECT_SOURCE_DIR))
 endif()
 
 # Eigen3 library
+find_package(Eigen3 REQUIRED)
 if(NOT TARGET Eigen3::Eigen)
     add_library(meshfem_eigen INTERFACE)
     meshfem_download_eigen()
@@ -76,72 +78,91 @@ if(NOT TARGET Eigen3::Eigen)
 endif()
 
 # json library
+find_package(nlohmann_json REQUIRED)
 if(NOT TARGET json::json)
-    add_library(meshfem_json INTERFACE)
-    meshfem_download_json()
-    target_include_directories(meshfem_json SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/json)
-    target_include_directories(meshfem_json SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/json/nlohmann)
-    add_library(json::json ALIAS meshfem_json)
+    if(TARGET nlohmann_json::nlohmann_json)
+        add_library(json::json ALIAS nlohmann_json::nlohmann_json)
+    else()
+        add_library(meshfem_json INTERFACE)
+        meshfem_download_json()
+        target_include_directories(meshfem_json SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/json)
+        target_include_directories(meshfem_json SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/json/nlohmann)
+        add_library(json::json ALIAS meshfem_json)
+    endif()
 endif()
 
 # Optional library
+find_package(optional-lite REQUIRED)
 if(NOT TARGET optional::optional)
-    meshfem_download_optional()
-    add_library(optional_lite INTERFACE)
-    target_include_directories(optional_lite SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/optional/include)
-    add_library(optional::optional ALIAS optional_lite)
+    if(TARGET nonstd::optional-lite)
+        add_library(optional::optional ALIAS nonstd::optional-lite)
+    else()
+        meshfem_download_optional()
+        add_library(optional_lite INTERFACE)
+        target_include_directories(optional_lite SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/optional/include)
+        add_library(optional::optional ALIAS optional_lite)
+    endif()
 endif()
 
 # TBB library
+find_package(TBB REQUIRED)
 if(MESHFEM_WITH_TBB AND NOT TARGET tbb::tbb)
-    set(TBB_BUILD_STATIC OFF CACHE BOOL " " FORCE)
-    set(TBB_BUILD_SHARED ON CACHE BOOL " " FORCE)
-    set(TBB_BUILD_TBBMALLOC ON CACHE BOOL " " FORCE) # needed for CGAL's parallel mesher
-    set(TBB_BUILD_TBBMALLOC_PROXY OFF CACHE BOOL " " FORCE)
-    set(TBB_BUILD_TESTS OFF CACHE BOOL " " FORCE)
+    if(TARGET onetbb::onetbb)
+        add_library(tbb::tbb ALIAS onetbb::onetbb)
+    else()
+        set(TBB_BUILD_STATIC OFF CACHE BOOL " " FORCE)
+        set(TBB_BUILD_SHARED ON CACHE BOOL " " FORCE)
+        set(TBB_BUILD_TBBMALLOC ON CACHE BOOL " " FORCE) # needed for CGAL's parallel mesher
+        set(TBB_BUILD_TBBMALLOC_PROXY OFF CACHE BOOL " " FORCE)
+        set(TBB_BUILD_TESTS OFF CACHE BOOL " " FORCE)
 
-    meshfem_download_tbb()
-    add_subdirectory(${MESHFEM_EXTERNAL}/tbb tbb EXCLUDE_FROM_ALL)
-    #set_property(TARGET tbb_static tbb_def_files PROPERTY FOLDER "dependencies")
-    #set_target_properties(tbb_static PROPERTIES COMPILE_FLAGS "-Wno-implicit-fallthrough -Wno-missing-field-initializers -Wno-unused-parameter -Wno-keyword-macro")
+        meshfem_download_tbb()
+        add_subdirectory(${MESHFEM_EXTERNAL}/tbb tbb EXCLUDE_FROM_ALL)
+        #set_property(TARGET tbb_static tbb_def_files PROPERTY FOLDER "dependencies")
+        #set_target_properties(tbb_static PROPERTIES COMPILE_FLAGS "-Wno-implicit-fallthrough -Wno-missing-field-initializers -Wno-unused-parameter -Wno-keyword-macro")
 
-    add_library(tbb_tbb INTERFACE)
-    target_include_directories(tbb_tbb SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/tbb/include)
-    target_link_libraries(tbb_tbb INTERFACE tbbmalloc tbb)
-    add_library(tbb::tbb ALIAS tbb_tbb)
-
-    meshfem_target_hide_warnings(tbb_tbb)
+        add_library(tbb_tbb INTERFACE)
+        target_include_directories(tbb_tbb SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/tbb/include)
+        target_link_libraries(tbb_tbb INTERFACE tbbmalloc tbb)
+        add_library(tbb::tbb ALIAS tbb_tbb)
+        meshfem_target_hide_warnings(tbb_tbb)
+    endif()
 endif()
 
 # Triangle library
 if(NOT TARGET triangle::triangle)
-    meshfem_download_triangle()
+    #meshfem_download_triangle()
     add_subdirectory(${MESHFEM_EXTERNAL}/triangle triangle)
     target_include_directories(triangle SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/triangle)
     add_library(triangle::triangle ALIAS triangle)
 endif()
 
 # Spectra library
+find_package(spectra REQUIRED)
 if(NOT TARGET spectra::spectra)
-    meshfem_download_spectra()
-    add_library(meshfem_spectra INTERFACE)
-    target_include_directories(meshfem_spectra SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/spectra/include)
-    add_library(meshfem::spectra ALIAS meshfem_spectra)
+    if(TARGET Spectra::Spectra)
+        add_library(meshfem::spectra ALIAS Spectra::Spectra)
+    else()
+        meshfem_download_spectra()
+        add_library(meshfem_spectra INTERFACE)
+        target_include_directories(meshfem_spectra SYSTEM INTERFACE ${MESHFEM_EXTERNAL}/spectra/include)
+        add_library(meshfem::spectra ALIAS meshfem_spectra)
+    endif()
 endif()
 
 # TinyExpr library
 if(NOT TARGET tinyexpr::tinyexpr)
-    meshfem_download_tinyexpr()
+    #meshfem_download_tinyexpr()
     add_library(meshfem_tinyexpr ${MESHFEM_EXTERNAL}/tinyexpr/tinyexpr.c)
     target_include_directories(meshfem_tinyexpr SYSTEM PUBLIC ${MESHFEM_EXTERNAL}/tinyexpr)
     add_library(tinyexpr::tinyexpr ALIAS meshfem_tinyexpr)
 endif()
 
 # Cholmod solver
-find_package(CHOLMOD REQUIRED) # provides cholmod::cholmod
+# find_package(CHOLMOD REQUIRED) # provides cholmod::cholmod
 
 # UmfPack solver
-find_package(UMFPACK REQUIRED) # provides umfpack::umfpack
+# find_package(UMFPACK REQUIRED) # provides umfpack::umfpack
 
 ################################################################################
 # Optional libraries
